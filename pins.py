f47fcc73de3340dd350c77795032979c696a4245
basilesanast1 <81961568+basilesanast1@users.noreply.github.com>
2026-06-02 14:00:18 +0300

    Update pins.py

--- pins.py
+++ pins.py
@@ -1,29 +1,33 @@
 import digitalio
 from digitalio import DigitalInOut, Pull
-from board import *
+import board
 from adafruit_debouncer import Debouncer
 
-#init button
-button1_pin = DigitalInOut(GP22) # defaults to input
-button1_pin.pull = Pull.UP      # turn on internal pull-up resistor
-button1 =  Debouncer(button1_pin)
+# -----------------------
+# BUTTON
+# -----------------------
+button1_pin = DigitalInOut(board.GP22)
+button1_pin.switch_to_input(pull=Pull.UP)
+button1 = Debouncer(button1_pin)
+button1.update()
 
+# -----------------------
+# PAYLOAD SELECT PINS
+# -----------------------
+payload1Pin = DigitalInOut(board.GP4)
+payload1Pin.switch_to_input(pull=Pull.UP)
 
-# payload1 = GPIO4 to GND
-# payload2 = GPIO5 to GND
-# payload3 = GPIO10 to GND
-# payload4 = GPIO11 to GND
+payload2Pin = DigitalInOut(board.GP5)
+payload2Pin.switch_to_input(pull=Pull.UP)
 
-#init payload selection switch
-payload1Pin = digitalio.DigitalInOut(GP4)
-payload1Pin.switch_to_input(pull=digitalio.Pull.UP)
-payload2Pin = digitalio.DigitalInOut(GP5)
-payload2Pin.switch_to_input(pull=digitalio.Pull.UP)
-payload3Pin = digitalio.DigitalInOut(GP10)
-payload3Pin.switch_to_input(pull=digitalio.Pull.UP)
-payload4Pin = digitalio.DigitalInOut(GP11)
-payload4Pin.switch_to_input(pull=digitalio.Pull.UP)
+payload3Pin = DigitalInOut(board.GP10)
+payload3Pin.switch_to_input(pull=Pull.UP)
 
-# check GP0 for setup mode
-progStatusPin = digitalio.DigitalInOut(GP0)
-progStatusPin.switch_to_input(pull=digitalio.Pull.UP)
+payload4Pin = DigitalInOut(board.GP11)
+payload4Pin.switch_to_input(pull=Pull.UP)
+
+# -----------------------
+# PROGRAM MODE PIN
+# -----------------------
+progStatusPin = DigitalInOut(board.GP0)
+progStatusPin.switch_to_input(pull=Pull.UP)
